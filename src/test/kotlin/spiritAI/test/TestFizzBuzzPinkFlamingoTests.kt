39fe2c4df2763442de10ba42f820a84f5068fe50
Natalia Oskina <natalia.oskina@zuehlke.com>
2018-06-10 00:35:00 +0100

    Add: test for numbers multiple 5

--- src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
+++ src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
@@ -8,10 +8,18 @@ class TestFizzBuzzPinkFlamingoTests {
 
 	@Test
 	fun returnFizzOnNumbersMultiple3() {
-		val expectedResult: MutableList<String> = mutableListOf("0", "1", "2","Fizz","4", "5")
+		val expectedResult: MutableList<String> = mutableListOf("0", "1", "2","Fizz","4")
+		val actualResult = FizzBuzzPinkFlamingo().getSequence(4)
+		Assert.assertTrue(expectedResult.equals(actualResult))
+	}
+
+	@Test
+	fun returnFizzOnNumbersMultiple5() {
+		val expectedResult: MutableList<String> = mutableListOf("0", "1", "2","Fizz", "4", "Buzz")
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(5)
 		Assert.assertTrue(expectedResult.equals(actualResult))
 	}
+
 
 }
 
