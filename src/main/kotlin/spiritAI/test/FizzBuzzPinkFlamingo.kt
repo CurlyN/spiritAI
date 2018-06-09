551e4188f9eb896dcfaf8feaf6d7193c70d6278b
Natalia Oskina <natalia.oskina@zuehlke.com>
2018-06-10 00:51:58 +0100

    Add: condition for numbers multiples of 3 and 5

--- src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
+++ src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
@@ -5,16 +5,17 @@ class FizzBuzzPinkFlamingo {
         val result = mutableListOf<String>()
 
         for (i in 0..lengthOfSequence) {
-           if (isMultipleToNumber(i, 3)) result.add("Fizz")
+           if (isMultipleToNumber(i, 15)) result.add("FizzBuzz")
+           else if (isMultipleToNumber(i, 3)) result.add("Fizz")
             else if (isMultipleToNumber(i, 5)) result.add("Buzz")
              else result.add(i.toString())
         }
         return result
     }
 
-    fun isMultipleToNumber(number: Int, devider: Int): Boolean{
+    fun isMultipleToNumber(number: Int, divider: Int): Boolean{
 
-        if (number!=0 && number % devider == 0) return true
+        if (number!=0 && number % divider == 0) return true
 
         return false
     }
--- src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
+++ src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
@@ -14,12 +14,22 @@ class TestFizzBuzzPinkFlamingoTests {
 	}
 
 	@Test
-	fun returnFizzOnNumbersMultiple5() {
+	fun returnBuzzOnNumbersMultiple5() {
 		val expectedResult: MutableList<String> = mutableListOf("0", "1", "2", "Fizz", "4", "Buzz")
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(5)
-		println(expectedResult)
-		println(actualResult)
 		Assert.assertTrue(expectedResult.equals(actualResult))
+	}
+
+	@Test
+	fun returnFizzBuzzOnNumbersMultiple3and5() {
+		val actualResult = FizzBuzzPinkFlamingo().getSequence(15)
+		Assert.assertTrue(actualResult[3].equals("Fizz"))
+		Assert.assertTrue(actualResult[5].equals("Buzz"))
+		Assert.assertTrue(actualResult[6].equals("Fizz"))
+		Assert.assertTrue(actualResult[9].equals("Fizz"))
+		Assert.assertTrue(actualResult[10].equals("Buzz"))
+		Assert.assertTrue(actualResult[12].equals("Fizz"))
+		Assert.assertTrue(actualResult[15].equals("FizzBuzz"))
 	}
 
 
