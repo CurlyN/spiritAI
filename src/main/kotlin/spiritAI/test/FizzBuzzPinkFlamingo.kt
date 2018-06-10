d20260139e96705ac649076d384ae88895fa7381
Natalia Oskina <natalia.oskina@zuehlke.com>
2018-06-10 01:56:22 +0100

    Refactor: main function to extract multiplication check

--- src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
+++ src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
@@ -5,16 +5,22 @@ class FizzBuzzPinkFlamingo {
         val result = mutableListOf<String>()
 
         for (i in 0..lengthOfSequence) {
-           if (isMultipleToNumber(i, 15)) result.add("FizzBuzz")
-           else if (isMultipleToNumber(i, 3)) result.add("Fizz")
-            else if (isMultipleToNumber(i, 5)) result.add("Buzz")
-             else result.add(i.toString())
+            val printFromCheckOfMultiple = checkForMultipleToNumber(i)
+            if (printFromCheckOfMultiple == "") result.add(i.toString())
+            else result.add(printFromCheckOfMultiple)
         }
+
         return result
     }
 
-    fun isMultipleToNumber(number: Int, divider: Int): Boolean{
+    fun checkForMultipleToNumber(number: Int): String{
+        if (isMultipleToNumber(number, 15)) return "FizzBuzz"
+        else if (isMultipleToNumber(number, 3)) return "Fizz"
+        else if (isMultipleToNumber(number, 5)) return "Buzz"
+        return ""
+    }
 
+    fun isMultipleToNumber(number: Int, divider: Int): Boolean{
         if (number!=0 && number % divider == 0) return true
 
         return false
