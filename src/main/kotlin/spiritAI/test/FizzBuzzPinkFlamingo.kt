38f98426dd32cfb5194515d74f77d266aec1d481
curlyN <nattly_osk@mail.ru>
2018-06-13 23:50:51 +0100

    Refactor: Kotlin usage in FizzBuzz

--- src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
+++ src/main/kotlin/spiritAI/test/FizzBuzzPinkFlamingo.kt
@@ -1,52 +1,57 @@
 package spiritAI.test
 
 class FizzBuzzPinkFlamingo {
-    fun getSequence(lengthOfSequence: Int): MutableList<String>{
-        val result = mutableListOf<String>()
+    fun getSequence(lengthOfSequence: Int): MutableList<String> {
+        // first number is 0 which we do not need to check
+        val result = mutableListOf("0")
 
-        for (i in 0..lengthOfSequence) {
-            //Perform checks of the number in the sequence
+        for (i in 1..lengthOfSequence) {
+            // Perform checks of the number in the sequence
             val checkResultOfFibonacci = checkForFibonacciNumber(i)
-            var checkResultOfMultiplication = checkForMultipleToNumber(i)
+            val checkResultOfMultiplication = checkForMultipleToNumber(i)
 
-            //Add space after FizzBuzz if the number is not Fibonacci
-            checkResultOfMultiplication = if (!checkResultOfFibonacci.equals("") && !checkResultOfMultiplication.equals(""))
-                checkResultOfMultiplication.plus(" ") else checkResultOfMultiplication
+            // Add space after FizzBuzz if the number is Fibonacci or return as a digit.
+            val resultPhrase =
+                if (!checkResultOfMultiplication.isNullOrBlank() && !checkResultOfFibonacci.isNullOrBlank()) {
+                    checkResultOfMultiplication.plus(" $checkResultOfFibonacci")
+                } else if (!checkResultOfMultiplication.isNullOrBlank() || !checkResultOfFibonacci.isNullOrBlank()) {
+                    checkResultOfMultiplication ?: checkResultOfFibonacci!!
+                } else {
+                    i.toString()
+                }
 
-            //Change FizzBuzzFlamingo into Pink Flamingo or return combined result
-            if (checkResultOfMultiplication.equals("") && checkResultOfFibonacci.equals("")) result.add(i.toString())
-            else if ((checkResultOfMultiplication + checkResultOfFibonacci)
-                    .equals("FizzBuzzFlamingo")) result.add("Pink Flamingo")
-            else result.add(checkResultOfMultiplication.plus(checkResultOfFibonacci))
+            // Change FizzBuzzFlamingo into Pink Flamingo or return combined result
+            // in the sequence 0..100 there are no numbers both fibonacci and multiple 15,
+            // however if the sequence is higher then we still check
+            if (resultPhrase == "FizzBuzz Flamingo") result.add("Pink Flamingo")
+            else result.add(resultPhrase)
         }
 
         return result
     }
 
-    fun checkForMultipleToNumber(number: Int): String{
+    fun checkForMultipleToNumber(number: Int): String? {
         if (isMultipleToNumber(number, 15)) return "FizzBuzz"
         else if (isMultipleToNumber(number, 3)) return "Fizz"
         else if (isMultipleToNumber(number, 5)) return "Buzz"
-        return ""
+        return null
     }
 
-    fun isMultipleToNumber(number: Int, divider: Int): Boolean{
-        return (number!=0 && number % divider == 0)
-    }
+    fun isMultipleToNumber(number: Int, divider: Int) =
+        number % divider == 0
 
-    fun checkForFibonacciNumber(number: Int): String{
+    fun checkForFibonacciNumber(number: Int): String? {
         if (isFibonacci(number)) return "Flamingo"
-        return ""
-    }
-    fun isFibonacci(number: Int): Boolean{
-        if (number  == 0) return false
-        return isPerfectSquare(5*number*number + 4) ||
-                isPerfectSquare(5*number*number - 4)
-    }
-    fun isPerfectSquare(number: Int): Boolean{
-        val s = kotlin.math.sqrt(number.toDouble()).toInt()
-        return (s*s == number)
+        return null
     }
 
+    fun isFibonacci(number: Int) =
+        isPerfectSquare(5 * number * number + 4) ||
+            isPerfectSquare(5 * number * number - 4)
 
+    fun isPerfectSquare(number: Int) =
+        number.let(Int::toDouble)
+            .let { kotlin.math.sqrt(it) }
+            .let(Double::toInt)
+            .let { it * it == number }
 }
--- src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
+++ src/test/kotlin/spiritAI/test/TestFizzBuzzPinkFlamingoTests.kt
@@ -5,32 +5,37 @@ import org.junit.Assert
 import org.junit.Test
 
 class TestFizzBuzzPinkFlamingoTests {
+    @Test
+    fun return0ForFirstChar(){
+        val actualResult = FizzBuzzPinkFlamingo().getSequence(5)
+        Assert.assertTrue(actualResult[0] == "0")
+    }
 
 	@Test
 	fun returnFizzOnNumbersMultiple3() {
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(9)
-		Assert.assertTrue(actualResult[9].equals("Fizz"))
+		Assert.assertTrue(actualResult[9] == "Fizz")
 	}
 
 	@Test
 	fun returnBuzzOnNumbersMultiple5() {
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(10)
-		Assert.assertTrue(actualResult[10].equals("Buzz"))
+		Assert.assertTrue(actualResult[10] == "Buzz")
 	}
 
 	@Test
 	fun returnFizzBuzzOnNumbersMultiple3and5() {
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(15)
-		Assert.assertTrue(actualResult[15].equals("FizzBuzz"))
+		Assert.assertTrue(actualResult[15] == "FizzBuzz")
 	}
 
 	@Test
 	fun returnFlamingoOnFibonacciNumbersAndMultiple3and5() {
 		val actualResult = FizzBuzzPinkFlamingo().getSequence(15)
-		Assert.assertTrue(actualResult[1].equals("Flamingo"))
-		Assert.assertTrue(actualResult[2].equals("Flamingo"))
-		Assert.assertTrue(actualResult[3].equals("Fizz Flamingo"))
-		Assert.assertTrue(actualResult[5].equals("Buzz Flamingo"))
+		Assert.assertTrue(actualResult[1] == "Flamingo")
+		Assert.assertTrue(actualResult[2] == "Flamingo")
+		Assert.assertTrue(actualResult[3] == "Fizz Flamingo")
+		Assert.assertTrue(actualResult[5] == "Buzz Flamingo")
 	}
 
 
